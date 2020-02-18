feb34684bc6066ba3006258f3c3a2616fe837940
Csepregi <csepregigabor02@gmail.com>
2020-02-18 22:27:59 +0100

    push frontend repo

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, waitForElement } from '@testing-library/react'
+jest.mock('./services/__mocks__/notes')
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+
+	test('renders all notes it gets from backend', async () => {
+		const component = render(
+			<App />
+		)
+		component.rerender(<App />)
+		await waitForElement(
+			() => component.container.querySelector('.note')
+		)
+
+		const notes = component.container.querySelectorAll('.note')
+		expect(notes.length).toBe(3)
+
+		expect(component.container).toHaveTextContent(
+			'HTML is easy'
+		)
+		expect(component.container).toHaveTextContent(
+			'Browser can execute only javascript'
+		)
+		expect(component.container).toHaveTextContent(
+			'The most important methods of HTTP are GET and POST'
+		)
+	})
+})
